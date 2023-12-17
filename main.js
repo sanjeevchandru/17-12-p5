874004b167e412299c649800a9bda4ed4fce498d
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-12-17 12:30:33 +0530

    17-12-p5

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("5 . to join all elements of the following array into a string "+"<br>");
+var a=["red","green","white","black"];
+document.write(a.toString()+"<br>");
+document.write(a.join()+"<br>");
+document.write(a.join("+")+"<br><br>");
